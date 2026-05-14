b250e5b8a7393295c7d54889a222ee8240301a1e
iantonovorg-create <i.antonov.org@kodland.org>
2026-05-14 16:41:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -371,7 +371,7 @@ app.post('/api/payroll', requireAuth, requireEditor, async (req, res) => {
         new_mot_repeat_plan,new_mot_repeat_fact,
         new_mot_first_plan,new_mot_first_fact,
         extra_bonuses,total,plan_days,fact_days)
-       VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20)
+       VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22)
        ON CONFLICT (id) DO UPDATE SET
          month=$2,employee_id=$3,name=$4,block=$5,sub=$6,salary=$7,bonus_type=$8,
          bonus_clients=$9,bonus_tickets=$10,bonus_chats=$11,
